31cea24ad9f6f3ef5662638c1181bec9dfa67d3b
WitNesS23 <250357981@qq.com>
2016-03-09 20:01:48 +0800

    update mobile-interface

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 module.exports = function(app){
 	app.get("/", function(req, res){
-		res.render("index", {
-			title: "常州职业信息技术学院校园招聘网站"
-		})
+		var userAgent = req.headers["user-agent"];
+		if(userAgent.match(/(iphone|ipod|ipad|android|MicroMessenger)/i)){
+			// Mobile or Wechat
+			res.render("index_Mobile", {
+				title: "常州职业信息技术学院校园招聘网站"
+			});			
+		}else{
+			// PC
+			res.render("index", {
+				title: "常州职业信息技术学院校园招聘网站"
+			});
+		}
 	});
 };
+
+function checkMobile(req, res, next){
+	var userAgent = req.headers["user-agent"];
+	if(userAgent.match(/(iphone|ipod|ipad|android|MicroMessenger)/i)){
+		
+	}
+}
